12defe72a44954611fae6948adecb7360bd72686
Anton Senko-Gaykevich <a.senko-gaykevich@itransition.com>
2021-03-07 23:45:03 +0200

    Added file-service and gateway-service

---
+++ src/main/kotlin/by/senko/gaykevich/gateway/GatewayApplication.kt
@@ -0,0 +1,11 @@
+package by.senko.gaykevich.gateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayApplication
+
+fun main() {
+	runApplication<GatewayApplication>()
+}
